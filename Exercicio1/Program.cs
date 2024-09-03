e77da08cad729f9ac7601b3af3c295170e863dd3
Artur Oliveira <arturolivv@gmail.com>
2024-09-02 21:11:59 -0300

    first commit

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[50];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            fibonacci[2] = 1;
+
+            for (int i = 3; i < fibonacci.Length; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1]; 
+            }
+
+            Console.Write("Digite um número para saber se ele está na sequência de Fibonacci: ");
+            int numeroInserido = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Sequência de Fibonacci " + string.Join(", ", fibonacci));
+            Console.WriteLine();
+
+            if (fibonacci.Contains(numeroInserido))
+            {
+                Console.WriteLine("O número inserido está presente na sequência de Fibonacci");
+            }
+            else
+            {
+                Console.WriteLine("O número inserido não está presente na sequência de Fibonacci");
+            }
+        }
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma string: ");
+        string input = Console.ReadLine();
+
+        int contador = 0;
+
+        foreach (char caracter in input)
+        {
+            if (caracter == 'a' || caracter == 'A')
+            {
+                contador++;
+            }
+        }
+
+        if (contador > 0)
+        {
+            Console.WriteLine($"A letra 'a' (maiúscula ou minúscula) foi encontrada {contador} vez(es) na string.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' (maiúscula ou minúscula) não foi encontrada na string.");
+        }
+    }
+}
